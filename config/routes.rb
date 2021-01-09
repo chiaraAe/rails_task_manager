0ab248f1eb0a8dc04549fa9f29ce5f0243de1812
Chiara Aengevelt <chiara@aengevelt.com>
2021-01-09 16:07:58 +0100

    Challenge completete without styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Show all Tasks
+  get '/tasks', to: 'tasks#index'
+
+  # Create a new task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: "tasks#create"
+
+  #Update task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  # Show 1 Task 
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2021_01_09_105332) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "details"
-    t.boolean "completed"
+    t.string "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
